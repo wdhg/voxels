3cb227200660d8fd3f67fbd25eb1881b901577df
wdhg <wdhgrant@gmail.com>
2021-02-18 17:25:40 +0000

    simplify octree code

--- src/octree.ts
+++ src/octree.ts
@@ -1,5 +1,3 @@
-import { Vector3 } from "three";
-
 /*
    0-------------1                       y
    |\            |\                      |
@@ -17,69 +15,47 @@ import { Vector3 } from "three";
 interface Node {
   tag: "node";
   children: Octree[];
-  position: Vector3;
-  size: number;
 }
 
 interface End {
   tag: "end";
-  value: boolean;
-  position: Vector3;
-  size: number;
 }
 
-export type Octree = Node | End;
+interface Empty {
+  tag: "empty";
+}
 
-const isNode = (tree: Octree): tree is Node => {
-  return (tree as Node).tag === "node";
-};
+export type Octree = Node | End | Empty;
 
-const isEnd = (tree: Octree): tree is End => {
-  return (tree as End).tag === "end";
-};
-
-const randomEnd = (position: Vector3, size: number): End => {
-  return {
-    tag: "end",
-    value: Math.random() < 0.5,
-    position: position,
-    size: size,
-  };
-};
-
-const offsetVector = (o: number): Vector3 =>
-  new Vector3(o & 0b001 ? 1 : 0, o & 0b010 ? 1 : 0, o & 0b100 ? 1 : 0);
-
-const randomNode = (position: Vector3, size: number): Node => {
-  const childSize = Math.floor(size / 2);
+const node = (children: Octree[]): Octree => {
   return {
     tag: "node",
-    children: Array.from(Array(8).keys()).map((v, _i, _a) =>
-      randomOctree(
-        offsetVector(v).multiplyScalar(childSize).add(position),
-        childSize
-      )
-    ),
-    position: position,
-    size: size,
+    children: children,
   };
 };
 
-export const randomOctree = (position: Vector3, size: number): Octree => {
-  if (size === 1 || Math.random() < 1 / size) {
-    return randomEnd(position, size);
-  }
-  return randomNode(position, size);
+const end = (): Octree => {
+  return { tag: "end" };
 };
 
-export const forEachVoxel = (
-  tree: Octree,
-  f: (pos: Vector3, size: number) => any
-) => {
-  if (isEnd(tree) && tree.value) {
-    console.log("voxel", tree.position, tree.size);
-    f(tree.position, tree.size);
-  } else if (isNode(tree)) {
-    tree.children.forEach((t, _i, _a) => forEachVoxel(t, f));
+const empty = (): Octree => {
+  return { tag: "empty" };
+};
+
+const randomNode = (size: number): Octree => {
+  const childSize = Math.floor(size / 2);
+  const children = Array.from(Array(8).keys()).map((v, _i, _a) =>
+    randomOctree(childSize)
+  );
+  return node(children);
+};
+
+export const randomOctree = (size: number): Octree => {
+  if (size === 1) {
+    if (Math.random() < 0.5) {
+      return end();
+    }
+    return empty();
   }
+  return randomNode(size);
 };
